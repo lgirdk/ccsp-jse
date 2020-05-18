06c98acf413572326cf3914d27e93db719530498
Melanie Rhianna Lewis <melanie.lewis@redembedded.com>
2020-05-18 12:07:35 +0100

    jse_xml: Added specific array handling.

--- source/jse_xml.c
+++ source/jse_xml.c
@@ -4,8 +4,34 @@
 #include "jse_xml.h"
 
 /**
+ * Iterates over an array added nodes for each element.
+ *
+ * @param ctx the duktape context.
+ * @param obj_idx the index of the object walk.
+ * @param node the root node.
+ * @param name the name to use for the child nodes.
+ */
+static void iterate_array(duk_context * ctx, duk_idx_t obj_idx, xmlNodePtr node, const char* name)
+{
+    duk_size_t len = 0;
+    duk_size_t idx = 0;
+
+    len = duk_get_length(ctx, obj_idx);
+    for (idx = 0; idx < len; idx ++)
+    {
+        xmlNodePtr child = xmlNewChild(node, NULL, BAD_CAST name, NULL);
+        if (child != NULL)
+        {
+            duk_get_prop_index(ctx, obj_idx, idx);
+            xmlNodeSetContent(child, BAD_CAST duk_safe_to_string(ctx, -1));
+            duk_pop(ctx);
+        }
+    }
+}
+
+/**
  * Recursively walk over a JavaScript object making an XML tree.
- * 
+ *
  * @param ctx the duktape context.
  * @param obj_idx the index of the object walk.
  * @param node the root node.
@@ -19,23 +45,30 @@ static void walk_object(duk_context * ctx, duk_idx_t obj_idx, xmlNodePtr node)
        the key at -2 and the value at -1 */
     while (duk_next(ctx, obj_idx, true))
     {
-        xmlNodePtr child = xmlNewChild(
-            node, NULL, BAD_CAST duk_safe_to_string(ctx, -2), NULL);
-        if (child != NULL)
+        if (duk_is_array(ctx, -1))
         {
-            if (duk_is_object(ctx, -1))
-            {
-                /* Recurse in to object */
-                walk_object(ctx, -1, child);
-            }
-            else
-            {
-                xmlNodeSetContent(child, BAD_CAST duk_safe_to_string(ctx, -1));
-            }
-
-            /* Pop the key and the value */
-            duk_pop_2(ctx);
+            iterate_array(ctx, -1, node, duk_safe_to_string(ctx, -2));
         }
+        else
+        {
+            xmlNodePtr child = xmlNewChild(
+                node, NULL, BAD_CAST duk_safe_to_string(ctx, -2), NULL);
+            if (child != NULL)
+            {
+                if (duk_is_object(ctx, -1))
+                {
+                    /* Recurse in to object */
+                    walk_object(ctx, -1, child);
+                }
+                else
+                {
+                    xmlNodeSetContent(child, BAD_CAST duk_safe_to_string(ctx, -1));
+                }
+            }
+        }
+
+        /* Pop the key and the value */
+        duk_pop_2(ctx);
     }
 
     /* Pop the enumerator */
@@ -66,6 +99,8 @@ static duk_ret_t do_objectToXMLString(duk_context * ctx)
 
             if (root != NULL)
             {
+                /* You can't do arrays of the root node. To include an array create a JS
+                   object eg: { name: [ val1, val2, val3 ] } */
                 if (duk_is_object(ctx, -1))
                 {
                     walk_object(ctx, -1, root);
